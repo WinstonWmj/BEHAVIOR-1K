ebae8ce02e5f8fad99292860024b9d4e26342ee4
WinstonWmj <983289917@qq.com>
2026-04-04 10:09:51 +0800

    mjwei: add two tasks's reward function (task1 and task10);

--- OmniGibson/omnigibson/reward_functions/__init__.py
+++ OmniGibson/omnigibson/reward_functions/__init__.py
@@ -2,10 +2,16 @@ from omnigibson.reward_functions.collision_reward import CollisionReward
 from omnigibson.reward_functions.grasp_reward import GraspReward
 from omnigibson.reward_functions.hanging_pictures_reward import HangingPicturesReward
 from omnigibson.reward_functions.make_microwave_popcorn_reward import MakeMicrowavePopcornReward
+from omnigibson.reward_functions.picking_up_trash_reward import PickingUpTrashReward
 from omnigibson.reward_functions.point_goal_reward import PointGoalReward
 from omnigibson.reward_functions.potential_reward import PotentialReward
 from omnigibson.reward_functions.reaching_goal_reward import ReachingGoalReward
 from omnigibson.reward_functions.reward_function_base import REGISTERED_REWARD_FUNCTIONS, BaseRewardFunction
+from omnigibson.reward_functions.sequential_task_reward import SequentialTaskReward
+from omnigibson.reward_functions.set_up_a_coffee_station_in_your_kitchen_reward import (
+    SetUpACoffeeStationInYourKitchenReward,
+)
+from omnigibson.reward_functions.turning_on_radio_reward import TurningOnRadioReward
 
 __all__ = [
     "BaseRewardFunction",
@@ -13,8 +19,11 @@ __all__ = [
     "GraspReward",
     "HangingPicturesReward",
     "MakeMicrowavePopcornReward",
+    "PickingUpTrashReward",
     "PointGoalReward",
     "PotentialReward",
     "ReachingGoalReward",
+    "SequentialTaskReward",
+    "SetUpACoffeeStationInYourKitchenReward",
     "REGISTERED_REWARD_FUNCTIONS",
 ]
